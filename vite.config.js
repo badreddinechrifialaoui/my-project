9f95d7c293b83057e8241e9677316b7873dca2e6
badreddinechrifialaoui <badreddinechrifialaoui@gmail.com>
2026-02-08 15:50:42 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/YOUR_REPO_NAME/", // <--- ADD THIS LINE (Replace YOUR_REPO_NAME)
+  base: '/', 
 })
